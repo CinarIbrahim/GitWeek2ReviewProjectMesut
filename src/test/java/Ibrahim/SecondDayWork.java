005ef314086cb65be791b234ee71b9e4e7ce324e
Mesut Ozturk <90611319+BugKillerMiller@users.noreply.github.com>
2021-12-26 22:49:30 +0300

    Revert "master"

--- src/test/java/Ibrahim/SecondDayWork.java
+++
@@ -1,7 +0,0 @@
-package Ibrahim;
-
-public class SecondDayWork {
-    public static void main(String[] args) {
-        System.out.println("Doing second day work");
-    }
-}
--- src/test/java/MesutWork/MesutWork.java
+++
@@ -1,7 +0,0 @@
-package MesutWork;
-
-public class MesutWork {
-    public static void main(String[] args) {
-        System.out.println("Ibrahim Çınar is king of the SDET world.");
-    }
-}
